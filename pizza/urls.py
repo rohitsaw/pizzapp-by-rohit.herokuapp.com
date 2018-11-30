db113cb7c4337e2636e15c21d84c488725ef7897
rohitsaw <rsaw409@gmail.com>
2018-11-30 23:30:12 +0530

    test

--- pizza/urls.py
+++ pizza/urls.py
@@ -17,16 +17,17 @@ from django.contrib import admin
 from django.urls import include, path
 
 
-from django.contrib.staticfiles.urls import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles.urls import static
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path("profile/", include("userprofile.urls", namespace='userprofile')),
     path("", include("orders.urls")),
     path("admin/", admin.site.urls)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
