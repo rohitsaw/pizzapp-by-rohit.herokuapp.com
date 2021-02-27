f2749378e495558af013dd230fa131a1fdad6b0a
Rohit Saw <rsaw409@gmail.com>
2021-02-27 21:23:25 +0530

    change setting.py

--- pizza/settings.py
+++ pizza/settings.py
@@ -129,3 +129,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
