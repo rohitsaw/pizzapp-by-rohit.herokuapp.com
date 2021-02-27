192d419743e9bc205e914a33e4e3a66d08ade250
Rohit Saw <rsaw409@gmail.com>
2021-02-27 20:42:49 +0530

    project change

--- pizza/settings.py
+++ pizza/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i0&iq&e9u9h6(4_7%pt2s9)f=c$kso=k$c$w@fi9215s=1q0^d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pizzapp-by-rohit.herokuapp.com']
+ALLOWED_HOSTS = ['pizzapp-by-rohit.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -125,9 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
